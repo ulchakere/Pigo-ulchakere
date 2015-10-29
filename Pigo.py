2215caadc3f386c581ad2b31c115eee9b3ff2f32
Eric Ulchaker <ericulchaker@Erics-MacBook-Air.local>
2015-10-29 09:08:02 -0400

    Added stop and fwd

--- Pigo.py
+++ Pigo.py
@@ -1,11 +1,32 @@
-#from gopigo import *
+from gopigo import *
+import time
 
 
 __author__ = 'ericulchaker'
 
 class Pigo:
 
-        def __init__(self):
-            print "I am very tired today."
+    isMoving = False
+    servoPos = 90
+
+
+    def __init__(self):
+        print "I am very tired today."
+
+    def stop(selfself):
+        self.isMoving = False
+        while stop() != 1:
+            time.sleep(.1)
+            print "I messed up :("
+
+    def fwd(self):
+        self.isMoving = True
+        while fwd() != 1:
+            time.sleep(.1)
+            Print "This is not possible."
+
 
 tina = Pigo()
+tina.fwd()
+time.sleep(2)
+tina.stop()
